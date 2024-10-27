6f2baede9e7d42992f88b6f70ba6780413853e60
davemins <davemins4125@naver.com>
2024-10-27 23:38:20 +0900

    약수의 합

---
+++ Lv1/약수의 합.py
@@ -0,0 +1,29 @@
+'''
+# 문제 이해
+
+
+# 발상
+
+
+# 복잡도
+
+
+'''
+def solution(n):
+	answer = 0
+	for i in range(1, n + 1):
+		if n % i == 0:
+			answer = answer + i
+	return answer
+
+print(solution(12))
+'''
+# 푼 시간
+
+
+# 채점
+
+
+# 느낀 점
+
+'''
