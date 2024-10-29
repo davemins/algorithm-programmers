8d144704bbb7e5449ca8884169c7fb1e259044c4
davemins <davemins4125@naver.com>
2024-10-29 11:21:38 +0900

    음양 더하기

--- Lv1/음양 더하기.py
+++ Lv1/음양 더하기.py
@@ -1,12 +1,12 @@
 '''
 # 문제 이해
-
+정수 배열과 부호 배열을 통해 결과를 구하기
 
 # 발상
-
+구현해보자.
 
 # 복잡도
-
+입력 값의 개수가 n일 때 O(n)의 시간복잡도를 가진다.
 
 '''
 def solution(absolutes, signs):
@@ -21,11 +21,11 @@ def solution(absolutes, signs):
     return answer
 '''
 # 푼 시간
-
+1분
 
 # 채점
-
+정답
 
 # 느낀 점
-
+없습니다.
 '''
