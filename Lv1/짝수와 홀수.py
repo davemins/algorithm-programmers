60fa1757d6a29c65b0d983b8bc03621bc4709458
davemins <davemins4125@naver.com>
2024-11-04 21:49:33 +0900

    짝수와 홀수

---
+++ Lv1/짝수와 홀수.py
@@ -0,0 +1,21 @@
+'''
+# 문제 이해
+짝수와 홀수를 판단하자.
+
+'''
+def solution(num):
+    if num % 2 != 0:
+        answer = "Odd"
+    else:
+        answer = "Even"
+    return answer
+'''
+# 푼 시간
+1분
+
+# 채점
+정답
+
+# 느낀 점
+없습니다.
+'''
