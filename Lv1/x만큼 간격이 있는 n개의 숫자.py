d8ac0cf9b879eb37f812545040c0148a7d77b0d5
davemins <davemins4125@naver.com>
2024-11-05 10:28:41 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Lv1/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,20 @@
+'''
+# 문제 이해
+x만큼 간격이 있는 n개의 숫자
+
+'''
+def solution(x, n):
+    answer = []
+    for i in range(1, n + 1):
+        answer.append(i * x)
+    return answer
+'''
+# 푼 시간
+1분
+
+# 채점
+정답
+
+# 느낀 점
+없습니다.
+'''
