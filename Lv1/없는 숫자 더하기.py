e1d0d3a4e823194982ca79378feeb5c0744c90da
davemins <davemins4125@naver.com>
2024-11-10 23:41:56 +0900

    없는 숫자 더하기

---
+++ Lv1/없는 숫자 더하기.py
@@ -0,0 +1,25 @@
+'''
+# 문제 이해
+없는 숫자 더하기
+
+'''
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+    answer.sort()
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
+
+'''
+# 푼 시간
+2분
+
+# 채점
+정답
+
+# 느낀 점
+없습니다.
+'''
