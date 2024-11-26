b9644203339c49e349197118aa404a86a2adf45a
davemins <davemins4125@naver.com>
2024-11-26 15:12:59 +0900

    가운데 글자 가져오기

--- Lv1/가운데 글자 가져오기.py
+++ Lv1/가운데 글자 가져오기.py
@@ -1,3 +1,8 @@
+'''
+# 문제 이해
+리스트를 잘 다뤄보자
+
+'''
 def solution(s):
     answer = ''
     if len(s) % 2 == 0:
@@ -5,3 +10,14 @@ def solution(s):
     else:
         answer = s[len(s) // 2]
     return answer
+
+'''
+# 푼 시간
+3분
+
+# 채점
+정답
+
+# 느낀 점
+없습니다.
+'''
