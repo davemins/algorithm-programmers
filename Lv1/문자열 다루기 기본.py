e7130a1a936e42c527693a9bda2101f43b79ea55
davemins <69416617+davemins@users.noreply.github.com>
2024-11-27 23:46:23 +0900

    문자열 다루기 기본

--- Lv1/문자열 다루기 기본.py
+++ Lv1/문자열 다루기 기본.py
@@ -1,3 +1,9 @@
+
+'''
+# 문제 이해
+문자열을 검사하자.
+
+'''
 def solution(s):
     answer = True
     if len(s) in [4, 6] and s.isnumeric():
@@ -5,3 +11,13 @@ def solution(s):
     else:
         answer = False
     return answer
+'''
+# 푼 시간
+1분
+
+# 채점
+정답
+
+# 느낀 점
+isnumeric() 처음 알았습니다.
+'''
